500dd6097298db399209da2fbcde5ce57aba61a4
GuilhermeMorais81 <guilherme.morais01@aluno.cps.sp.gov.br>
2026-04-14 09:29:53 -0300

    feat(p1): da vida ao personagem e permite coleta de itens

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Jogo {
-   public static void main(String[] args) {
-      
+   public static final String SEPARADOR = "\n**********************";
+   public static void main(String[] args) throws Exception {
+      var gerador = new Random();
+      Personagem john = new Personagem("John", 10, 0, 0);
+      while(true) {
+         switch (gerador.nextInt(1,4)) {
+            case 1:
+               john.cacar();
+               break;
+            case 2:
+               john.comer();
+               break;
+            case 3:
+               john.dormir();
+               break;
+         }
+         System.out.println(john);
+         System.out.println(SEPARADOR);
+         Thread.sleep(5000);
+      }
    }
 }
