3e187fe11650c8890c6e43a599027024395528d7
GuilhermeMorais81 <guilherme.morais01@aluno.cps.sp.gov.br>
2026-04-14 20:35:22 -0300

    feat(p1): adiciona personagem

--- src/Jogo.java
+++ src/Jogo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Jogo {
-   public static final String SEPARADOR = "\n**********************";
+   public static final String SEPARADOR = "\n**********************\n";
    public static void main(String[] args) throws Exception {
 
       ArrayList<Musica> musicas = new ArrayList<>();
@@ -17,36 +17,48 @@ public class Jogo {
       musicas.add(new Musica("Thousand Knives"));
       musicas.add(new Musica("Beauty and the Beast"));
 
-
       var gerador = new Random();
-      boolean continuar = true;
       Personagem john = new Personagem("John", 3, 0, 0);
-      while(continuar) {
-         switch (gerador.nextInt(1,5)) {
-            case 1:
-               john.cacar();
-               break;
-            case 2:
-               john.comer();
-               break;
-            case 3:
-               john.dormir();
-               break;
-            case 4:
-               john.aprenderMusica(musicas);
-         }
+      ArrayList<Musica> musicasTicoTrovador = new ArrayList<>();
+      musicasTicoTrovador.add(new Musica("Vento do Norte"));
+      Personagem ticoTrovador = new Personagem("Tico Trovador", 3, 8, 8, musicasTicoTrovador);
+
+      while(john.isVivo() || ticoTrovador.isVivo()) {
          if(john.isVivo()) {
+            switch (gerador.nextInt(1,5)) {
+               case 1:
+                  john.cacar();
+                  break;
+               case 2:
+                  john.comer();
+                  break;
+               case 3:
+                  john.dormir();
+                  break;
+               case 4:
+                  john.aprenderMusica(musicas);
+            }
             System.out.println(john);
-            System.out.println(SEPARADOR);
-            Thread.sleep(5000);
-         }
-         else {
-            System.out.println("--> " + john.getNome() + " ESTA MORTO.");
-            System.out.println("\nInventario Final:\n" + john.exibirInventario());
-            System.out.println("\nRepertorio Final:\n" + john.exibirRepertorio());
-            continuar = false;
+            System.out.println("\n");
          }
          
+         if(ticoTrovador.isVivo()) {
+            ticoTrovador.aprenderMusica(musicas);
+            switch (gerador.nextInt(1,11)) {
+               case 1, 2:
+                  ticoTrovador.cacar();
+                  break;
+               case 3, 4:
+                  ticoTrovador.comer();
+                  break;
+               case 5, 6, 7, 8, 9, 10:
+                  ticoTrovador.dormir();
+                  break;
+            }
+            System.out.println(ticoTrovador);
+         }
+         System.out.println(SEPARADOR);
+         Thread.sleep(5000);
       }
    }
 }
